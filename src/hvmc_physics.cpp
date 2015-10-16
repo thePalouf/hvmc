7dbf3cab5a3a99986c56b5110fefced4f3007db4
thePalouf <20902652@u3s112-12.mig.snap.fsi>
2015-10-16 14:50:08 +0200

    update ridig bodies i guess

--- src/hvmc_physics.cpp
+++ src/hvmc_physics.cpp
@@ -2,10 +2,20 @@
 
 void RigidBody::Update( f32 dt )
 {
+    /*
+     *notes funky, mais apparemment on s'en tape de update de rigidbody
+    ai = 1/m * Somme(Fi);
+    v(i+1) = vi + dt*ai;
+    x(i+1) = xi + dt*v(i+1);
+    */
+    vec2 acc = im * forces;
+    velocity += dt*acc;
+    position += dt*velocity;
 }
 
 void RigidBody::ApplyForce( vec2 const& f )
 {
+    forces+=f;
 }
 
 void RigidBody::ApplyImpulse( vec2 const& impulse, vec2 const& contactVector )
@@ -78,5 +88,38 @@ RigidBody* PhysicsSystem::AddWall( vec2 const& pos, vec2 const& dims )
 
 void PhysicsSystem::Update( f32 dt )
 {    
+    //add gravity
+    for (RigidBody* rb : rigidBodies){
+        rb->ApplyForce(rb->m * gravity);
+        rb->Update(dt);
+    }
+
+    //generate contact infos
+    /*for (RigidBody* rb1 : rigidBodies){
+        for (RigidBody* rb2 : rigidBodies){
+
+              CollisionInfo info;
+              if ( Collider(a,b,info)
+                collisions.push_back(info);
+
+        }
+
+         //Integrate Forces
+         for (RigidBody* rb : rigidBodies){
+            rb->IntegrateForces(dt); // linearVelocity+=a*dt;
+                                     // angularVelocity += theta*dt;
+         }
+         //integrate velocities
+         for(){
+            rb.integratevelocities(dt); //position+=v*dt; rotation+=w*dt;
+         }
+         //clear forces
+         for(){
+             rb.forces = {0.0,0.0};
+             rb.torque = 0.0;
+         }
+
+    }
+    }*/
 }
 
