645ba3ec09166df19dc2d6d5d2d9cf3865508477
thePalouf <benjiz65@hotmail.com>
2015-10-19 14:16:41 +0200

    Added headers for collisions but not sure about the struct Collider in physics

--- src/hvmc_collisions.cpp
+++ src/hvmc_collisions.cpp
@@ -1,1 +1,36 @@
 #include "hvmc_collisions.h"
+
+//test collision between a and b, gives an info
+bool Collisions::collider(RigidBody * a, RigidBody * b, CollisionInfo * info){
+    //circle2circle
+    if ((a->collider.type == RIGID_BODY_SPHERE) && (b->collider.type == RIGID_BODY_SPHERE)){
+        return collisionCircle2Circle(a,b,info);
+    }
+    //box2box
+    if ((a->collider.type == RIGID_BODY_BOX) && (b->collider.type == RIGID_BODY_BOX)){
+        return collisionBox2Box(a,b,info);
+    }
+
+    //box2circle
+    if ((a->collider.type == RIGID_BODY_SPHERE) && (b->collider.type == RIGID_BODY_BOX)){
+        return collisionBox2Circle(b,a,info);
+    }
+    else if ((a->collider.type == RIGID_BODY_BOX) && (b->collider.type == RIGID_BODY_SPHERE)){
+        return collisionBox2Circle(a,b,info);
+    }
+}
+
+//test collision box to box
+bool Collisions::collisionBox2Box(RigidBody * box1, RigidBody * box2, CollisionInfo * info){
+    return false;
+}
+
+//test collision circle to circle
+bool Collisions::collisionCircle2Circle(RigidBody * circle1, RigidBody * circle2, CollisionInfo * info){
+    return false;
+}
+
+//test collision box to circle
+bool Collisions::collisionBox2Circle(RigidBody * box, RigidBody * circle, CollisionInfo * info){
+    return false;
+}
--- src/hvmc_collisions.h
+++ src/hvmc_collisions.h
@@ -2,6 +2,29 @@
 #define HVMC_COLLISIONS_H
 
 #include "hvmc_math.h"
+#include "hvmc_physics.h"
 
 #endif
+
+struct CollisionInfo{
+    //be my guest
+    //add whatever you want
+};
+
+struct Collisions{
+    //test collision between a and b, gives an info
+    bool collider(RigidBody * a, RigidBody * b, CollisionInfo * info);
+
+    //test collision box to box
+    bool collisionBox2Box(RigidBody * box1, RigidBody * box2);
+
+    //test collision circle to circle
+    bool collisionCircle2Circle(RigidBody * circle1, RigidBody * circle2);
+
+    //test collision box to circle
+    bool collisionBox2Circle(RigidBody * box, RigidBody * circle);
+
+
+    std::vector<CollisionInfo*> collisions; //collisions.push_back(info);
+};
 
